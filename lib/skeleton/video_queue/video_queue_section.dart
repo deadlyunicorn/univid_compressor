5842e33b20367661fb1bb3317a9b56d77df755e9
deadlyunicorn <petrache.dev@gmail.com>
2024-03-07 20:33:32 +0200

    minor adjustments to VideoQueueSection height and spacings

--- lib/skeleton/video_queue/video_queue_section.dart
+++ lib/skeleton/video_queue/video_queue_section.dart
@@ -1,4 +1,7 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
+import "package:univid_compressor/core/constants.dart";
 import "package:univid_compressor/core/widgets.dart";
 import "package:univid_compressor/skeleton/video_queue/completed/completed_processing_list.dart";
 import "package:univid_compressor/skeleton/video_queue/pending/video_queue_list.dart";
@@ -12,10 +15,10 @@ class VideoQueueSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.7,
-      child: const RowWithSpacings(
+      height: max( MediaQuery.sizeOf(context).height * 0.7, kMediumScreenWidth),
+      child: const RowWithSpacings( 
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        spacing: 32,
+        spacing: 16,
         children: <Widget>[
           VideoQueueList(),
           StartConvertingSelectedButton(),
