d1c694792bb6eab54ee40fea725f5e15656584b6
deadlyunicorn <petrache.dev@gmail.com>
2024-03-07 20:33:14 +0200

    added a CustomContainer Widget. also made the app somewhat responsive for showcasing flutter's ability to build for any screen to my cousin rofl

--- lib/skeleton/video_queue/list_container.dart
+++ lib/skeleton/video_queue/list_container.dart
@@ -12,19 +12,38 @@ class ListContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: kMediumScreenWidth,
+      width: MediaQuery.sizeOf(context).width / 2 < kMediumScreenWidth
+          ? kSmallScreenWidth
+          : kMediumScreenWidth,
       child: Expanded(
         child: Padding(
           ///? Leave space for Stack() overlaying eleents
           padding: const EdgeInsets.all(32.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: child,
-          ),
+          child: CustomContainer(child: child),
         ),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+        child: child,
       ),
     );
   }
