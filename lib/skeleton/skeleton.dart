50ed1d513aeb6c46fa82318aa8176f935a006921
deadlyunicorn <petrache.dev@gmail.com>
2024-04-01 19:17:21 +0300

    Added a ChangeNotifierProvider inside skeleton that decides wether to display the setup or the mainApp

--- lib/skeleton/skeleton.dart
+++ lib/skeleton/skeleton.dart
@@ -1,30 +1,30 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "package:univid_compressor/core/business/ffmpeg_helper.dart";
 import "package:univid_compressor/core/constants.dart";
-import "package:univid_compressor/core/widgets.dart";
-import "package:univid_compressor/skeleton/settings/compression_settings_section.dart";
-import "package:univid_compressor/skeleton/video_queue/video_queue_section.dart";
+import "package:univid_compressor/skeleton/main_app.dart";
+import "package:univid_compressor/skeleton/setup/setup_ffmpeg.dart";
 
 class Skeleton extends StatelessWidget {
   const Skeleton({super.key});
 
   @override
   Widget build(BuildContext context) {
-  
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Center(
           child: SizedBox(
             width: k3XLScreenWidth,
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 16),
-              child: ColumnWithSpacings(
-                spacing: 16,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  VideoQueueSection(),
-                  CompressionSettingsSection(),
-                ],
+              padding:
+                  const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16),
+              child: ChangeNotifierProvider<FFMpegController>(
+                create: (BuildContext context) => FFMpegController.initialize(),
+                builder: (BuildContext context, Widget? child) {
+                  return context.watch<FFMpegController>().binaryExists
+                      ? const MainApp()
+                      : const SetupFFMpeg();
+                },
               ),
             ),
           ),
