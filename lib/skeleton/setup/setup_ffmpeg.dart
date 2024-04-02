926bf20d0a1f63297815c0a777fed825882fb863
deadlyunicorn <admin@deadlyunicorn.dev>
2024-04-02 18:45:58 +0300

    setup FFMPEG shows the Widget based on FFmpegController errorMessage

--- lib/skeleton/setup/setup_ffmpeg.dart
+++ lib/skeleton/setup/setup_ffmpeg.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "package:univid_compressor/core/business/ffmpeg_helper.dart";
 import "package:univid_compressor/core/widgets.dart";
 import "package:univid_compressor/skeleton/setup/download_ffmpeg.dart";
 
@@ -9,18 +11,46 @@ class SetupFFMpeg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height,
-      child: const Center(
-        child: ColumnWithSpacings(
-          spacing: 16,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("FFMpeg doesn't seem to exist "),
-            DownloadFFMpeg(),
-          ],
-        ),
-      ),
+    final SetupError? setupErrorMessage =
+        context.select<FFMpegController, SetupError?>(
+      (FFMpegController controller) => controller.setupError,
     );
+
+    switch (setupErrorMessage) {
+      //! Not implemented for Windows, MacOS etc.
+      case SetupError.invalidPlatform:
+        return SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          child: Center(
+            child: Text(
+              "Your platform is not supported.",
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+            ),
+          ),
+        );
+      //* No errors - YET.
+      default:
+        return SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          child: const Center(
+            child: ColumnWithSpacings(
+              spacing: 16,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  "FFMpeg doesn't seem to exist.\n"
+                  "You can install it manually and then restart the app.\n\n"
+                  "If your system's architecture is x86_64\n"
+                  " we can install the static binaries for you.",
+                  textAlign: TextAlign.center,
+                ),
+                DownloadFFMpeg(),
+              ],
+            ),
+          ),
+        );
+    }
   }
 }
