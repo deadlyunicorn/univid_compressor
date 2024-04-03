aa22f40159409abc4ebfa9cdbd1795271a5e8afd
deadlyunicorn <admin@deadlyunicorn.dev>
2024-04-03 03:21:13 +0300

    Updated ImportedVideos. Now we have Thumbnails etc. ( unfinished )

--- lib/skeleton/video_queue/pending/video_container/video_preview_container.dart
+++ lib/skeleton/video_queue/pending/video_container/video_preview_container.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+
 import "package:flutter/material.dart";
 import "package:univid_compressor/core/constants.dart";
 import "package:univid_compressor/core/video_details.dart";
@@ -24,44 +26,59 @@ class VideoPreviewContainer extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Stack(
+          child: ColumnWithSpacings(
+            spacing: 8,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              ColumnWithSpacings(
-                spacing: 8,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      height: kMediumContainerWidth,
-                      width: kLargeContainerWidth,
-                    ),
-                  ),
-                  Tooltip(
-                    message: videoDetails.name,
-                    child: Text(videoDetails.fileNameShort),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      videoDetails.sizeString,
-                    ),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Thumbnail(videoDetails: videoDetails),
               ),
-              Positioned(
-                left: 0,
-                top: 0,
-                child: IconButton(
-                  color: Theme.of(context).colorScheme.error,
-                  onPressed: () {
-                    removeSelf();
-                  },
-                  icon: const Icon(Icons.delete),
+              Tooltip(
+                message: videoDetails.name,
+                child: Text(videoDetails.fileNameShort),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      color: Theme.of(context).colorScheme.error,
+                      onPressed: () {
+                        removeSelf();
+                      },
+                      icon: const Icon(Icons.delete),
+                    ),
+                    RowWithSpacings(
+                      spacing: 4,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            //? Ideas: For Linux download the binary and dependencies and execute ffmpeg commands based on it.
+                            // FFmpegKit.execute(
+                            //     "-i ${videoDetails.fileReference.path}");
+                          },
+                          child: const Text(
+                            "FFMPEG",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        Tooltip(
+                          textAlign: TextAlign.center,
+                          message:
+                              "hello world", //TODO Display video details here
+                          child: Icon(
+                            color: Theme.of(context).colorScheme.surface,
+                            Icons.info,
+                          ),
+                        ),
+                        Text(
+                          videoDetails.sizeString,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -74,4 +91,43 @@ class VideoPreviewContainer extends StatelessWidget {
   //TODO Future builder that gets the thumbnail
   //TODO Video record date, length, resolution
   //TODO Maybe we can hover over thumbnail and dislpay those ( if we don't playback)
+}
+
+class Thumbnail extends StatelessWidget {
+  const Thumbnail({
+    required this.videoDetails,
+    super.key,
+  });
+
+  final VideoDetails videoDetails;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      height: kMediumContainerWidth,
+      width: kLargeContainerWidth,
+      child: Image(
+        image: FileImage(
+          File(
+            "/home/deadlyunicorn/Pictures/circle.jpg", //TODO future builder that gets the thumbnail from video using FFMPEG
+          ),
+        ),
+        frameBuilder: (
+          BuildContext context,
+          Widget child,
+          int? frame,
+          bool wasSynchronouslyLoaded,
+        ) =>
+            frame != null
+                ? child
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+      ),
+    );
+  }
 }
